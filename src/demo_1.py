dd78b1ecebf2b3e15f517aae261ecb1e5f26c829
lixingcong <lixingcong@live.com>
2016-11-25 20:34:06 +0800

    demo_1: pre-process: blur

--- src/demo_1.py
+++ src/demo_1.py
@@ -9,19 +9,24 @@ import cv2
 from matplotlib import pyplot as plt
 import sys
 
-img = cv2.imread("../data/4x3.png")
+img = cv2.imread("../data/s.png")
 img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
-ret,thresh1 = cv2.threshold(img_gray,127,255,cv2.THRESH_BINARY)
-print img.shape
-print img[0,2]
-print len(img[0])
-print len(img)
-sys.exit(0)
+img_resized = cv2.resize(img, (48, 48), interpolation=cv2.INTER_CUBIC)
+cv2.imwrite('../data/s_resized.png',img_resized)
 
-cv2.imshow('image', thresh1)
-cv2.waitKey(0)
+img_resized=cv2.medianBlur(img_resized,1)
+img_blur=cv2.bilateralFilter(img_resized,-1,20,20)
+
+# ret,thresh1 = cv2.threshold(img_gray,127,255,cv2.THRESH_BINARY)
+
+cv2.imshow('image1', img_resized)
+cv2.imshow('image2', img_blur)
+# cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+cv2.imwrite('../data/s_blur.png',img_blur)
+
 
 # plt.figure("figure_111")
 # plt.imshow(img, 'gray')
