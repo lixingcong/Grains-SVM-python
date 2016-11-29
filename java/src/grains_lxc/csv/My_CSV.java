ff48bacab70d3caa75cb93421e203a141e725751
lixingcong <lixingcong@live.com>
2016-11-29 15:26:17 +0800

    csv.java: function prototype

---
+++ java/src/grains_lxc/csv/My_CSV.java
@@ -0,0 +1,48 @@
+/*
+ * Date: 2016年11月29日
+ * Author: li
+ * https://www.mkyong.com/java/how-to-read-and-parse-csv-file-in-java/
+ */
+package grains_lxc.csv;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class My_CSV {
+
+	String csv_file;
+	String line = "";
+	String cvsSplitBy = ",";
+	BufferedReader br = null;
+	List<String> content_read=null;
+        
+	public My_CSV(String csv_file){
+		this.csv_file=csv_file;
+	}
+	
+	private void read_all() {
+		if (content_read==null){
+			content_read=new ArrayList<String>();	
+		
+			try (BufferedReader br = new BufferedReader(new FileReader(csv_file))) {
+				while ((line = br.readLine()) != null) {
+					content_read.add(line);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public List<String> read(){
+		read_all();
+		return content_read;
+	}
+	
+	public int get_total_rows(){
+		return content_read.size();
+	}
+}
