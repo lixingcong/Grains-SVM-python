2664bc091c6117ed06cda3c2cc0ecacf0b281dc6
lixingcong <lixingcong@live.com>
2016-11-29 15:06:59 +0800

    java: new project folder

---
+++ java/src/offical_demos/OfficalOpenCVSample.java
@@ -0,0 +1,28 @@
+/*
+ * Date: 2016年11月29日
+ * Author: li
+ * http://docs.opencv.org/2.4/doc/tutorials/introduction/desktop_java/java_dev_intro.html
+ */
+package offical_demos;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.core.CvType;
+
+public class OfficalOpenCVSample {
+
+	static {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to OpenCV " + Core.VERSION);
+		Mat m = new Mat(5, 10, CvType.CV_8UC1, new Scalar(0));
+		System.out.println("OpenCV Mat: " + m);
+		Mat mr1 = m.row(1);
+		mr1.setTo(new Scalar(1));
+		Mat mc5 = m.col(5);
+		mc5.setTo(new Scalar(5));
+		System.out.println("OpenCV Mat data:\n" + m.dump());
+	}
+}
