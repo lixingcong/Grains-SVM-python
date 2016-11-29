fddcdf85bd51443954936d7079da2ab2e9731f66
lixingcong <lixingcong@live.com>
2016-11-29 19:58:00 +0800

    svm.java: train ok

---
+++ java/src/grains_lxc/svm/Demo.java
@@ -0,0 +1,27 @@
+/*
+ * Date: 2016年11月29日
+ * Author: li
+ */
+package grains_lxc.svm;
+
+import java.util.ArrayList;
+import java.util.List;
+import grains_lxc.features.My_Features;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		My_SVM my_svm=new My_SVM(1.0,0.1);
+		My_Features features=new My_Features("/tmp/1.csv");
+		
+		features.load_saved_features();
+		
+		List<List<Float>> res_x=features.get_features_x();
+		List<Float> res_y=features.get_features_y();
+		
+//		System.out.print(res_x);
+		my_svm.train(res_x, res_y);
+		//my_svm.predict(res_x);
+	}
+
+}
--- java/src/grains_lxc/svm/My_SVM.java
+++ java/src/grains_lxc/svm/My_SVM.java
@@ -4,78 +4,62 @@
  */
 package grains_lxc.svm;
 import grains_lxc.stat_model.StatModel;
+import grains_lxc.features.My_Features;
+
+import java.util.List;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
 import org.opencv.ml.CvSVM;
+import org.opencv.ml.CvSVMParams;
 
 public class My_SVM extends StatModel{
-
-	CvSVM model_svm=null;
 	
-	public My_SVM(){
-		model_svm=new CvSVM();
+	static {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	}
+
+	private CvSVM model_svm=null;
+	private String trained_model_filename = "cv2_svm_model.xml";
+	
+	private CvSVMParams my_svm_params= new CvSVMParams();
+	
+	public My_SVM(double svm_param_C,double svm_param_gamma){
+		this.model_svm=new CvSVM();
+		this.my_svm_params.set_svm_type(CvSVM.C_SVC);
+		this.my_svm_params.set_kernel_type(CvSVM.RBF);
+		this.my_svm_params.set_C(svm_param_C);
+		this.my_svm_params.set_gamma(svm_param_gamma);
 	}
 	
+	public void train(List<List<Float>> train_x, List<Float> train_y){
+		int item_features_qty=train_x.get(0).size();
+		int item_qty=train_y.size();
+		
+		Mat mat_train_x=new Mat(item_qty,item_features_qty,CvType.CV_32FC1);
+		Mat mat_train_y=new Mat(item_qty,1,CvType.CV_32FC1);
+		
+		// 填充数据到Mat类型中
+		for(int i=0;i<item_qty;i++){
+			mat_train_y.put(i,0,train_y.get(i).floatValue());
+			
+			for(int j=0;j<item_features_qty;j++){
+				mat_train_x.put(i,j,train_x.get(i).get(j).floatValue());
+			}
+		}
+		
+		model_svm.train(mat_train_x, mat_train_y, new Mat(), new Mat(), my_svm_params);
+	}
+		
 	@Override
 	public void load(String filename) {
+		model_svm.load(filename);
 	}
 
 	@Override
 	public void save(String filename) {
-
+		model_svm.save(filename);
 	}
-	
-	// http://stackoverflow.com/questions/23776610/svm-prediction-of-images-opencv
-	public void sss() {
-		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-
-		Mat classes = new Mat();
-		Mat trainingData = new Mat();
-		Mat trainingImages = new Mat();
-		Mat trainingLabels = new Mat();
-		CvSVM clasificador;
-		String path = "C:\\java workspace\\ora\\images\\Color_Happy_jpg";
-		for (File file : new File(path).listFiles()) {
-			Mat img = new Mat();
-			Mat con = Highgui.imread(path + "\\" + file.getName(), Highgui.CV_LOAD_IMAGE_GRAYSCALE);
-			con.convertTo(img, CvType.CV_32FC1, 1.0 / 255.0);
-
-			img.reshape(1, 1);
-			trainingImages.push_back(img);
-			trainingLabels.push_back(Mat.ones(new Size(1, 75), CvType.CV_32FC1));
-
-		}
-		System.out.println("divide");
-		path = "C:\\java workspace\\ora\\images\\Color_Sad_jpg";
-		for (File file : new File(path).listFiles()) {
-			Mat img = new Mat();
-			Mat m = new Mat(new Size(640, 480), CvType.CV_32FC1);
-			Mat con = Highgui.imread(file.getAbsolutePath(), Highgui.CV_LOAD_IMAGE_GRAYSCALE);
-
-			con.convertTo(img, CvType.CV_32FC1, 1.0 / 255.0);
-			img.reshape(1, 1);
-			trainingImages.push_back(img);
-
-			trainingLabels.push_back(Mat.zeros(new Size(1, 75), CvType.CV_32FC1));
-
-		}
-
-		trainingLabels.copyTo(classes);
-		CvSVMParams params = new CvSVMParams();
-		params.set_kernel_type(CvSVM.LINEAR);
-		CvType.typeToString(trainingImages.type());
-		CvSVM svm = new CvSVM();
-
-		clasificador = new CvSVM(trainingImages, classes, new Mat(), new Mat(), params);
-
-		clasificador.save("C:\\java workspace\\ora\\images\\svm.xml");
-		Mat out = new Mat();
-
-		clasificador.load("C:\\java workspace\\ora\\images\\svm.xml");
-		Mat sample = Highgui.imread("C:\\java workspace\\ora\\images\\Color_Sad_jpg\\EMBfemale20-2happy.jpg",
-				Highgui.CV_LOAD_IMAGE_GRAYSCALE);
-
-		sample.convertTo(out, CvType.CV_32FC1, 1.0 / 255.0);
-		out.reshape(1, 75);
-		System.out.println(clasificador.predict(out));
-	}
-	
 }
