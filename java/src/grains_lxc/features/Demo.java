aacbdced48c3a64deb24ff7f2ec04619674d964a
lixingcong <lixingcong@live.com>
2016-12-02 15:40:44 +0800

    features.java: load & save

---
+++ java/src/grains_lxc/features/Demo.java
@@ -0,0 +1,25 @@
+/*
+ * Date: 2016年12月1日
+ * Author: li
+ */
+package grains_lxc.features;
+
+import java.util.List;
+
+public class Demo {
+	public static void main(String[] args) {
+		My_Features f = new My_Features("../data/test_list.csv","../data/test_features.csv");
+		f.load_saved_features();
+		f.save_features("/tmp/1.csv");
+		// System.out.println(f.get_chinese_from_category(1));
+
+//		List<List<Double>> res_x = f.get_features_x();
+//		List<Double> res_y = f.get_features_y();
+//
+//		for (int i = 0; i < res_y.size(); i++) {
+//			System.out.print(res_y.get(i));
+//			System.out.print(": ");
+//			System.out.println(res_x.get(i));
+//		}
+	}
+}
--- java/src/grains_lxc/features/My_Features.java
+++ java/src/grains_lxc/features/My_Features.java
@@ -9,7 +9,17 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+import grains_lxc.crop.My_Crop;
 import grains_lxc.csv.My_CSV;
+import grains_lxc.preprocess.My_Preprocess;
+import grains_lxc.rilbp.My_RILBP;
+import grains_lxc.shape.My_Shape;
+import grains_lxc.utils.My_Utils;
 
 /*
  * CSV: splited by common
@@ -28,15 +38,25 @@ import grains_lxc.csv.My_CSV;
 public class My_Features {
 
 	String csv_features_save;
+	String csv_itemlist;
 	String csv_split_char = ",";
 
-	List<List<Double>> x = new ArrayList<List<Double>>();
-	List<Double> y = new ArrayList<Double>();
-	Map<String, String> dict_category_to_chinese = new HashMap<String, String>();
-	List<String> features = null;
+	private List<List<Double>> x = new ArrayList<List<Double>>();
+	private List<Double> y = new ArrayList<Double>();
+	private Map<String, String> dict_category_to_chinese = new HashMap<String, String>();
+	private List<String> features = null;
+	private List<String> itemlist=null;
+	
+	private My_RILBP myrilbp=null;
+	private My_Crop mycrop=null;
+	private My_Utils myutils=null;
 
-	public My_Features(String csv_features_save) {
+	public My_Features(String csv_itemlist, String csv_features_save) {
 		this.csv_features_save = csv_features_save;
+		this.csv_itemlist=csv_itemlist;
+		this.mycrop=new My_Crop(3,3,false);
+		this.myrilbp=new My_RILBP();
+		this.myutils=new My_Utils();
 	}
 
 	public void load_saved_features() {
@@ -47,6 +67,15 @@ public class My_Features {
 			dict_category_to_chinese.put(line_splited[1], line_splited[0]);
 		}
 		load_y_x_from_features();
+	}
+	
+	public void save_features(){
+		this.save_features(csv_features_save);
+	}
+	
+	public void save_features(String csv_filename){
+		My_CSV my_csv = new My_CSV(csv_filename);
+		my_csv.write(features);
 	}
 
 	private void load_y_x_from_features() {
@@ -63,9 +92,87 @@ public class My_Features {
 			y.add(new Double(line_splited.get(1)));
 		}
 	}
+	
+	private void generate_features_from_y_x(){
+		features=new ArrayList<String>();
+		for(int i=0;i<y.size();i++){
+			Integer this_y=new Integer(new Double(y.get(i)).intValue());
+			int category_int=this_y.intValue();
+			String category_str=this_y.toString();
+			String this_line="";
+			this_line+=get_chinese_from_category(category_int)+",";
+			this_line+=category_str+",";
+			for(Double this_feature:x.get(i)){
+				this_line+=this_feature.toString()+",";
+			}
+			features.add(this_line);
+		}
+	}
 
 	public String get_chinese_from_category(int c) {
 		return dict_category_to_chinese.get(Integer.toString(c));
+	}
+	
+	public void load_itemlist(){
+		My_CSV my_csv = new My_CSV(this.csv_itemlist);
+		itemlist=my_csv.read();
+		for(String line:itemlist){
+			String[] this_line_spilted=line.split(this.csv_split_char);
+			dict_category_to_chinese.put(this_line_spilted[1], this_line_spilted[0]);
+		}
+		for(int i=0;i<itemlist.size();i++)
+			calc_a_features_from_itemlist(i);
+		
+		// normalize
+		normalize_x();
+	}
+	
+	private void calc_a_features_from_itemlist(int index){
+		String this_line=itemlist.get(index);
+		String[] this_line_spilted=this_line.split(this.csv_split_char);
+		
+		// skip item which has an abnormal flag
+		if(this_line_spilted[3]=="0")
+			return;
+		
+		String filename=this_line_spilted[2];
+		calc_feature_from_filename(filename,Double.parseDouble(this_line_spilted[1]));
+	}
+	
+	private void calc_feature_from_filename(String filename,double this_y){
+		List<Double> this_feature=null;
+		
+		My_Preprocess mypreprocess=new My_Preprocess(filename);
+		
+		// RGB features
+		double[] RGB=myutils.get_rgb_normolized(mypreprocess.get_img(), mypreprocess.get_img_binary());
+		this_feature.add(RGB[0]);
+		this_feature.add(RGB[1]);
+		
+		// Hu(1)
+		My_Shape myshape=new My_Shape(mypreprocess.get_img_gray());
+		double Hu_1=myshape.get_humoments();
+		this_feature.add(Hu_1);
+		
+		// LBP
+		Mat img_foreground=myshape.get_foreground();
+		Mat img_resized=new Mat();		
+		Size s = new Size(48.0,48.0);
+		Imgproc.resize(img_foreground, img_resized, s, 0, 0, Imgproc.INTER_CUBIC);
+		List<Mat> img_splited=mycrop.get_cropped_images(img_resized);
+		
+		for(Mat img_:img_splited){
+			List<Double> lbp_histogram=myrilbp.get_lbp_histogram(img_);
+			this_feature.addAll(lbp_histogram);
+		}
+		
+		// add to y and x
+		x.add(this_feature);
+		y.add(this_y);
+	}
+	
+	private void normalize_x(){
+		x=myutils.normalize_from_list(x);
 	}
 
 	public List<Double> get_features_y() {
@@ -76,19 +183,5 @@ public class My_Features {
 		return x;
 	}
 
-	public static void main(String[] args) {
-		My_Features f = new My_Features("../data/test_features.csv");
-		f.load_saved_features();
-		// System.out.println(f.get_chinese_from_category(1));
 
-		List<List<Double>> res_x = f.get_features_x();
-		List<Double> res_y = f.get_features_y();
-
-		for (int i = 0; i < res_y.size(); i++) {
-			System.out.print(res_y.get(i));
-			System.out.print(": ");
-			System.out.println(res_x.get(i));
-		}
-
-	}
 }
